fab2a7a569f86cb6ef5c9c342dc264a351e7157a
Nick Dimiduk <ndimiduk@gmail.com>
2012-03-10 18:36:20 -0800

    fix dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject HBaseIA "1.0.0-SNAPSHOT"
   :description "Code and examples pertaining to HBase In Action"
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "1.0.0"]
-                     [org.apache.hbase/hbase "0.90.4"]]
+  :dependencies [[org.apache.hbase/hbase "0.90.5"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "1.0.0"]]
   :omit-source true
   :javac-options {:debug "true"
                   :target "1.5"}
   :source-path "src/main/clj"
   :java-source-path "src/main/java"
-  :run-aliases {:users-tool HBaseIA.TwitBase.cli.UsersTool
-                :twits-tool HBaseIA.TwitBase.cli.TwitsTool
+  :run-aliases {:users-tool  HBaseIA.TwitBase.cli.UsersTool
+                :twits-tool  HBaseIA.TwitBase.cli.TwitsTool
                 :init-tables HBaseIA.TwitBase.cli.InitTables})
