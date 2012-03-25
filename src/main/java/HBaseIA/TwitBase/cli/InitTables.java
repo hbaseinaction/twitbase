b479395121d706649f6ef17416b27718bf3869d2
Nick Dimiduk <ndimiduk@gmail.com>
2012-03-24 17:01:27 -0700

    clean up logging. add -f to InitTables

--- src/main/java/HBaseIA/TwitBase/cli/InitTables.java
+++ src/main/java/HBaseIA/TwitBase/cli/InitTables.java
@@ -7,15 +7,39 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import HBaseIA.TwitBase.hbase.TwitsDAO;
 import HBaseIA.TwitBase.hbase.UsersDAO;
 
 public class InitTables {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     Configuration conf = HBaseConfiguration.create();
     HBaseAdmin admin = new HBaseAdmin(conf);
+
+    // first do no harm
+    if (args.length > 0 && args[0].equalsIgnoreCase("-f")) {
+      System.out.println("!!! dropping tables in...");
+      for (int i = 5; i > 0; i--) {
+        System.out.println(i);
+        Thread.sleep(1000);
+      }
+
+      if (admin.tableExists(UsersDAO.TABLE_NAME)) {
+        System.out.printf("Deleting %s\n", Bytes.toString(UsersDAO.TABLE_NAME));
+        if (admin.isTableEnabled(UsersDAO.TABLE_NAME))
+          admin.disableTable(UsersDAO.TABLE_NAME);
+        admin.deleteTable(UsersDAO.TABLE_NAME);
+      }
+
+      if (admin.tableExists(TwitsDAO.TABLE_NAME)) {
+        System.out.printf("Deleting %s\n", Bytes.toString(TwitsDAO.TABLE_NAME));
+        if (admin.isTableEnabled(TwitsDAO.TABLE_NAME))
+          admin.disableTable(TwitsDAO.TABLE_NAME);
+        admin.deleteTable(TwitsDAO.TABLE_NAME);
+      }
+    }
 
     if (admin.tableExists(UsersDAO.TABLE_NAME)) {
       System.out.println("User table already exisis.");
